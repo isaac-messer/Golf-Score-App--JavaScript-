6e9b2d3aef466e402e576316c15fa8f2a1f514f5
Isaac Messer <isaacmesser@Philip.local>
2023-03-24 19:37:05 -0600

    Finished Filling table with Api Data

--- active.js
+++ active.js
@@ -1,64 +1,83 @@
 const inputForm = document.querySelector('[data-create-form]')
 const formSubmitButton = document.querySelector('[data-from-submit-button]');
-const scoreCardContainer = document.querySelector('[data-score-card-container]')
 
 let courseSelectDropdown = document.querySelector('[data-course-dropdown]');
 let playerNameInput = document.querySelector('[data-player-name]');
 let playerSkillDropdown = document.querySelector('[data-player-Skill-dropdown]');
-let currentPlayerName = playerName;
 
-// Gets User Input Data
+let currentPlayers = []
+let currentCourse = null;
+
+
+// Gets User Data from input form on userInfo.html
 function onCourseId() {
     let newCourseId = courseSelectDropdown.value
+    // console.log(newCourseId);
     return newCourseId;
 }
     function onCourseName() {
         let newCourseName = courseSelectDropdown.options[courseSelectDropdown.selectedIndex].text;
+        // console.log(newCourseName);
         return newCourseName;
     }
         function onPlayerName() {
             let newPlayerName = playerNameInput.value;
+            // console.log(newPlayerName);
             return newPlayerName;
         }
             function onPlayerSkill() {
                 let newPlayerSkill = playerSkillDropdown.options[playerSkillDropdown.selectedIndex].text;
+                // console.log(newPlayerSkill);
                 return newPlayerSkill;
             }
+            function onPlayerSkillValue() {
+                let newPlayerSkillValue = playerSkillDropdown.value;
+                // console.log(newPlayerSkillValue);
+                return newPlayerSkillValue
+            }
+                function onPlayerId() {
+                    let newPlayerId = Date.now();
+                    // console.log(newPlayerId);
+                    return newPlayerId;
+                }
 
-// Prevents page reload, and runs RenderFunctions. As well as saves user info to local storage 
+
+// Inserts form information into arrays from easier organization for multiple players
+function addPlayer(playerName, playerSkill, playerSkillValue, playerId) {
+    const newPlayer = {
+        name: playerName,
+        skill: playerSkill,
+        skillValue: playerSkillValue,
+        id: playerId,
+        score: Array(0).fill(0),
+    }
+    
+    currentPlayers.push(newPlayer);
+}
+    function selectedCourseInfo(courseName, courseId) {
+        const selectedCourse = {
+            name: courseName,
+            id: courseId,
+        }
+
+        currentCourse = selectedCourse;
+    }
+
+
+// Prevents page reload, and saves user info to local storage 
 inputForm.addEventListener('submit', function(event) {
     event.preventDefault();
 })
     formSubmitButton.addEventListener('click', function() {
+        addPlayer(onPlayerName(), onPlayerSkill(), onPlayerSkillValue(), onPlayerId());
+        selectedCourseInfo(onCourseName(), onCourseId());
+        
         console.log('Form Submitted');
+        console.log(currentPlayers);
+        console.log(currentCourse);
 
-        localStorage.setItem('playerName', onPlayerName());
-        localStorage.setItem('playerSkill', onPlayerSkill());
-        localStorage.setItem('courseName', onCourseName());
-        localStorage.setItem('courseId', onCourseId());
+        localStorage.setItem('activePlayers', JSON.stringify(currentPlayers));
+        localStorage.setItem('activeCourse', JSON.stringify(currentCourse));
 
         setTimeout(window.location.href = 'index.html', 500)
     })
-
-// These RenderFunctions inserts user input data onto Document
-
-function renderPage() {
-    renderHeadingPlayerInfo(localStorage.getItem('playerName'), localStorage.getItem('playerSkill'));
-    renderHeadingPlayerInfo(localStorage.getItem('courseName'), localStorage.getItem('courseId'));
-}
-function renderHeadingPlayerInfo(playerName, playerSkillLevel) {
-    console.log(playerName, playerSkillLevel);
-    let playerHeader = document.createElement('h1');
-    let playerHeaderText = document.createTextNode(`${playerName} - ${playerSkillLevel}`);
-
-    playerHeader.appendChild(playerHeaderText);
-    scoreCardContainer.appendChild(playerHeader);
-}
-    function renderHeadingCourseInfo(courseName, courseId) {
-        console.log(courseName, courseId);
-        let courseHeader = document.createElement('h3');
-        let courseHeaderText = document.createTextNode(`${courseName}: id-${courseId}`);
-
-        courseHeader.appendChild(courseHeaderText);
-        scoreCardContainer.appendChild(courseHeader);
-    }
